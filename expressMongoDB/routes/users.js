3d5195197176456aeb4391ee9dd9f2fca515a2bc
codeWithShubhamS <codewithshubhams@gmail.com>
2025-07-16 22:51:30 +0530

    MongoDB Express Application

--- expressMongoDB/routes/users.js
+++ expressMongoDB/routes/users.js
@@ -13,4 +13,55 @@ router.post("/", async (req, res) => {
   }
 })
 
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find()
+    res.send(users)
+  } catch(err) {
+    res.status(500).send(err.message)
+  }
+})
+
+// Get a user by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      return res.status(404).send("User not found")
+    }
+    res.send(user)
+  } catch(err) {
+    res.status(404).send(err.message)
+  }
+})
+
+// Update a user by ID
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    if (!user) {
+      return res.status(404).send("User not found")
+    }
+    res.send(user)
+  } catch(err) {
+    res.status(400).send(err.message)
+  }
+})
+
+// Delete a user by ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) {
+      return res.status(404).send("User not found")
+    }
+    res.send(user)
+  } catch(err) {
+    res.status(400).send(err.message)
+  }
+})
+
+
 module.exports = router;
